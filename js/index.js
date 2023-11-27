fc8672f8170f9fb40a0bec7ef65f57dcb3ff55c0
alfreddai <ya95123@gmail.com>
2023-11-27 16:24:25 +0800

    feat(js): initial completion of bannerImgsEffect

--- js/index.js
+++ js/index.js
@@ -2,44 +2,46 @@ const banner = document.getElementById("aggregation-banner");
 const bannerImgs = document.querySelectorAll(".aggregation__banner-img");
 const bannerImgsPositionInfo = [];
 
-// const getTranslatePercent = (item) => {
-//   let x = item.dataset.x;
-//   let y = item.dataset.y;
-
-//   item.setAttribute("style", `transform: translate(${x}%,${y}%);`);
-// };
-
-// bannerImgs.forEach((item) => {
-//   getTranslatePercent(item);
-// });
-
-const bannerImgsPosition = (item) => {
-  let x = Number(item.dataset.x);
-  let y = Number(item.dataset.y);
-  let distanceTargetX = x + 50; // x-(-50)
-  let distanceTargetY = y + 50; // y-(-50)
-
-  item.setAttribute(
-    "style",
-    `transform: translate(calc(${x}vw - 50%),calc(${y}vw - 50%));`
-  );
-  console.log(distanceTargetX, distanceTargetY);
-};
-
-bannerImgs.forEach((item) => {
-  bannerImgsPosition(item);
-});
-
-window.addEventListener("scroll", (e) => {
-  const bannerEffectHeight = Math.ceil(banner.scrollHeight * 0.75);
-  const scrollY = window.scrollY;
-  // console.log(scrollY);
-  // console.log(bannerEffectHeight);
+const bannerImgsEffect = () => {
+  const scrollY = window.scrollY; // 頁面滾動到頂部的高度
+  const bannerEffectHeight = Math.ceil(banner.scrollHeight * 0.75); // banner effect 的有效高度
 
   if (scrollY <= bannerEffectHeight) {
-    let scrollPersent = Math.ceil((scrollY / bannerEffectHeight) * 100);
-    // console.log(scrollPersent);
-  }
-});
+    let scrollRatio = scrollY / bannerEffectHeight; // 在 banner effect 有效區的滾動比例
 
-const bannerScrollEffect = (persent) => {};
+    bannerImgs.forEach((item, idx) => {
+      let x = bannerImgsPositionInfo[idx].x;
+      let y = bannerImgsPositionInfo[idx].y;
+      let dynamicX = x - x * scrollRatio; // ex. 20vw*1%=2vw -> 20vw-2vw=18vw
+      let dynamicY = y - y * scrollRatio; // ex. -20vw*1%=-2vw -> -20vw-(-2vw)=-18vw
+
+      item.setAttribute(
+        "style",
+        `transform: translate(calc(${dynamicX}vw - 50%),calc(${dynamicY}vw - 50%));`
+      );
+
+      // console.log(dynamicX, dynamicY);
+    });
+  } else {
+    bannerImgs.forEach((item) => {
+      item.setAttribute("style", `transform: translate(-50%,-50%);`);
+    });
+  }
+};
+
+const bannerImgsPosition = () => {
+  bannerImgs.forEach((item) => {
+    let x = Number(item.dataset.x);
+    let y = Number(item.dataset.y);
+
+    bannerImgsPositionInfo.push({
+      x: x,
+      y: y,
+    });
+  });
+
+  bannerImgsEffect();
+};
+
+bannerImgsPosition();
+window.addEventListener("scroll", bannerImgsEffect);
