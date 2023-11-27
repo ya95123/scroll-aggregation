9efe9f53cd665f9e4f8dca0500f9aa1f2292e9bd
alfreddai <ya95123@gmail.com>
2023-11-27 14:56:19 +0800

    perf(js): to calculate transform

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const banner = document.getElementById("aggregation-banner");
 const bannerImgs = document.querySelectorAll(".aggregation__banner-img");
+const bannerImgsPositionInfo = [];
 
 // const getTranslatePercent = (item) => {
 //   let x = item.dataset.x;
@@ -12,27 +13,33 @@ const bannerImgs = document.querySelectorAll(".aggregation__banner-img");
 //   getTranslatePercent(item);
 // });
 
-const test = (item) => {
+const bannerImgsPosition = (item) => {
   let x = Number(item.dataset.x);
   let y = Number(item.dataset.y);
   let distanceTargetX = x + 50; // x-(-50)
   let distanceTargetY = y + 50; // y-(-50)
 
-  item.setAttribute("style", `transform: translate(${x}%,${y}%);`);
+  item.setAttribute(
+    "style",
+    `transform: translate(calc(${x}vw - 50%),calc(${y}vw - 50%));`
+  );
   console.log(distanceTargetX, distanceTargetY);
 };
 
 bannerImgs.forEach((item) => {
-  test(item);
+  bannerImgsPosition(item);
 });
 
 window.addEventListener("scroll", (e) => {
-  const bannerEffectHeight = Math.floor(banner.scrollHeight * 0.75);
+  const bannerEffectHeight = Math.ceil(banner.scrollHeight * 0.75);
   const scrollY = window.scrollY;
   // console.log(scrollY);
   // console.log(bannerEffectHeight);
 
   if (scrollY <= bannerEffectHeight) {
-    console.log("I'm in.");
+    let scrollPersent = Math.ceil((scrollY / bannerEffectHeight) * 100);
+    // console.log(scrollPersent);
   }
 });
+
+const bannerScrollEffect = (persent) => {};
